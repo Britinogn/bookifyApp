190adb696ab49ce65e52a754c150f798f0df81b6
Tino Nova Tech <124672998+Britinogn@users.noreply.github.com>
2026-01-18 04:22:09 +0100

    chore: Update default API base URL to the Render deployment URL.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import { headConfig, seoConfig } from './app/config/seo.config'
 
-const API_BASE_URL = import.meta.env.API_BASE_URL || 'http://localhost:8080'
-//const API_BASE_URL = import.meta.env.API_BASE_URL || 'https://rest-api-go-bnnn.onrender.com'
+//const API_BASE_URL = import.meta.env.API_BASE_URL || 'http://localhost:8080'
+const API_BASE_URL = import.meta.env.API_BASE_URL || 'https://rest-api-go-bnnn.onrender.com'
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -39,5 +39,5 @@ export default defineNuxtConfig({
         ...headConfig
       }
   },
-  
+
 })
