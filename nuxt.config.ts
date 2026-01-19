2ea92d9fc723bc0fe9a01abf4efe940d02a2bf71
Tino Nova Tech <124672998+Britinogn@users.noreply.github.com>
2026-01-19 04:58:32 +0100

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import { headConfig, seoConfig } from './app/config/seo.config'
 
-const API_BASE_URL = process.env.NUXT_PUBLIC_API_BASE || 'https://rest-api-go-bnnn.onrender.com'
-//const API_BASE_URL = process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8080'
+//const API_BASE_URL = process.env.NUXT_PUBLIC_API_BASE || 'https://rest-api-go-bnnn.onrender.com'
+const API_BASE_URL = process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8080'
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
